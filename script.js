a264e9767e6c32a896dfb91dcaef54a3d09f3b5a
AngelI4 <168652607+AngelI4@users.noreply.github.com>
2024-05-29 14:17:58 +0000

    Unit1-02-HTML

---
+++ script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Angel  All rights reserved
+//
+// Created by: Angel
+// Created on: May 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() { 
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "I coded this in Javascript!"
+}
